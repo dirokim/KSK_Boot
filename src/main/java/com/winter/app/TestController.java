4eabadaa739d717b3cd4449d6df45613a7c63499
dirokim <dirokim7@gmail.com>
2024-03-07 18:42:01 +0900

    feat AOP

--- src/main/java/com/winter/app/TestController.java
+++ src/main/java/com/winter/app/TestController.java
@@ -4,24 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.winter.app.transfer.Bus;
-import com.winter.app.transfer.Subway;
+
 
 import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class TestController {
-	@Autowired
-	private Bus bus;
-	@Autowired
-	private Subway subway;
+
 	
 	@GetMapping("/")
 	public String test () {
 		//trace , debug, info ,warn ,error	
-		subway.getSubway(10);
-		bus.getBus();
-		
+
 		return "index";
 	}
 	
