0255c3ee65f33db8ad9b4e5dfd862a062ff39d6e
dirokim <dirokim7@gmail.com>
2024-03-11 10:40:19 +0900

    feat schedule

---
+++ src/main/java/com/winter/app/schedule/TestSchedule.java
@@ -0,0 +1,28 @@
+package com.winter.app.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.winter.app.member.MemberDAO;
+//@Component
+public class TestSchedule {
+	
+	
+//	@Scheduled(fixedRate = 1000,initialDelay = 2000)
+//	@Scheduled(fixedRateString = "1000",initialDelayString = "1000")
+//	public void useFixRate () {
+//		System.out.println("fix rate");
+//	}
+//	
+//	@Scheduled(fixedDelayString = "2000",initialDelayString = "1000")
+//	public void useFixDelay() {
+//		System.out.println("fix delay");
+//	}
+//	
+//	@Scheduled(cron = "*/10 * * * * *")
+//	public void useCrone() {
+//		
+//		System.out.println("cron delay");
+//	}
+}
